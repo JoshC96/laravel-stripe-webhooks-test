6c5f1b58595b3f8fe933725b9a016b05dbeea886
Josh Campbell <josh.campbell4000@gmail.com>
2023-10-24 15:33:55 +1030

    update CustomerRepository to utilize DB transactions and add findByStripeId method

--- app/Repositories/CustomerRepository.php
+++ app/Repositories/CustomerRepository.php
@@ -3,37 +3,66 @@
 namespace App\Repositories;
 
 use App\Models\Customer;
-use Illuminate\Database\Eloquent\MassAssignmentException;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
 use InvalidArgumentException;
-use Illuminate\Database\Eloquent\InvalidCastException;
 
 class CustomerRepository
 {
 
     /**
+     * @param string $stripeId 
+     * @return Customer 
+     * @throws InvalidArgumentException 
+     * @throws ModelNotFoundException 
+     */
+    public function findByStripeId(string $stripeId): Customer
+    {
+        return Customer::query()->where(Customer::FIELD_STRIPE_ID, $stripeId)->firstOrFail();
+    }
+
+
+    /**
      * @param array $data 
-     * @return Customer
+     * @return Customer 
+     * @throws Exception 
      */
     public function createCustomer(array $data): Customer
     {
-        $customer = new Customer();
-        $customer->fill($data);
-        $customer->save();
+        try { 
+            DB::beginTransaction();
+            $customer = new Customer();
+            $customer->fill($data);
+            $customer->save();
+            DB::commit();
 
-        return $customer;
+            return $customer;
+        } catch (Exception $exception) {
+            DB::rollBack();
+            throw $exception;
+        }         
     }
 
     /**
+     * 
      * @param Customer $customer 
      * @param array $data 
      * @return bool 
-     * @throws MassAssignmentException 
-     * @throws InvalidArgumentException 
-     * @throws InvalidCastException 
+     * @throws Exception 
      */
     public function updateCustomer(Customer $customer, array $data): bool
     {
-        $customer->fill($data);
-        return $customer->save();
+        try {
+            DB::beginTransaction();
+            $customer->fill($data);
+            return $customer->save();
+            DB::commit();
+
+            return $customer;
+        } catch (Exception $exception) {
+            DB::rollBack();
+            throw $exception;
+        }             
     }
 }
