d1b371dd6122f010e7ae94a253e1241e9f9bd35e
Josh Campbell <josh.campbell4000@gmail.com>
2023-10-24 13:38:40 +1030

    update customer phone field to be nullable

--- database/migrations/2023_10_24_014412_create_customers_and_subscriptions_tables.php
+++ database/migrations/2023_10_24_014412_create_customers_and_subscriptions_tables.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone')->unique();
+            $table->string('phone')->unique()->nullable();
             $table->integer('status')->default(0);
 
             $table->unsignedBigInteger('user_id')->nullable();
