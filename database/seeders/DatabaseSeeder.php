56724ec3e66d1baf87e8d6249597e935c55091db
Josh Campbell <josh.campbell4000@gmail.com>
2023-10-24 16:08:50 +1030

    update DatabaseSeeder class to generate users, subscriptions, and customers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,32 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Customer;
+use App\Models\CustomerSubscription;
+use App\Models\Invoice;
+use App\Models\Subscription;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Subscription::factory(5);
+
+        Customer::factory(5)
+            ->has(CustomerSubscription::factory()->count(5), Customer::RELATION_CUSTOMER_SUBSCRIPTIONS)
+            ->has(Invoice::factory()->count(5), Invoice::RELATION_CUSTOMER)
+            ->create();
+
     }
 }
