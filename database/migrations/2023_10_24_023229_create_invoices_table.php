d9bae9977837dc7cdf1b9baa3fb81f7291ec0992
Josh Campbell <josh.campbell4000@gmail.com>
2023-10-24 17:06:55 +1030

    add missing soft deletes from invoice migration

--- database/migrations/2023_10_24_023229_create_invoices_table.php
+++ database/migrations/2023_10_24_023229_create_invoices_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
 
             $table->integer('status')->default(0);
             $table->integer('total_price'); // stored as cents
